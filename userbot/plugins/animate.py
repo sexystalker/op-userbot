f087b110b6bfe6a97d1a4180fe6051ff6536fc0d
Online_Promoter <54389828+OnlinePromoter@users.noreply.github.com>
2020-03-28 20:51:53 +0530

    Update animate.py

--- userbot/plugins/animate.py
+++ userbot/plugins/animate.py
@@ -7,6 +7,7 @@ Available Commands:
 .up
 .round
 .anim
+.lub
 .fnl
 .monkey
 .hand
@@ -241,6 +242,246 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 11])
+
+
+
+"""Emoji
+Available Commands:
+.load
+.square
+.up
+.round
+.anim
+.fnl
+.monkey
+.hand
+.herber"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "load":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "▮",
+
+            "▯",
+
+            "▬",
+
+            "▭"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+"""Emoji
+Available Commands:
+.emoji shrug
+.emoji apple
+.emoji :/
+.emoji -_-"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "square":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "◧",
+
+            "◨",
+
+            "◧",
+
+            "◨"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "up":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "╹",
+
+            "╻",
+
+            "╹",
+
+            "╻"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "round":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "⚫",
+
+            "⬤",
+
+            "●",
+
+            "∘"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 11)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "lub":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "😍",
+
+            "😘",
+
+            
+
+            
+            "‎**Repo by @Online_Promoter**",
+ 
+            "😍",
+
+            "😘",
+
+
+            
+
+            
+
+            "‎github.com/Online_Promoter/op-userbot",
+
+            "__**@Online_Promoter Is BeHiNd You....**__"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 11])
+
 
 
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
